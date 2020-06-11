90fcc9bd1402828ca027fe1d3f47d2ce5f3e32e0
slagga <957766762@qq.com>
2020-06-11 17:31:01 +0800

    add websocket market

---
+++ examples/websocket_test.go
@@ -0,0 +1,133 @@
+package examples
+
+import (
+	"EX_binancequant/trade/futures"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func Test_Depth(t *testing.T) {
+	wsDepthHandler := func(event *futures.WsDepthEvent) {
+		fmt.Println(event.Asks[0].Price)
+		fmt.Println(event.Asks[0].Quantity)
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, stopC, err := futures.WsDepthServe("BTCUSDT", wsDepthHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// use stopC to exit
+	go func() {
+		time.Sleep(9527 * time.Hour)
+		stopC <- struct{}{}
+	}()
+	// remove this if you do not want to be blocked here
+	<-doneC
+}
+
+func Test_PartialDepth(t *testing.T) {
+	wsDepthHandler := func(event *futures.WsDepthEvent) {
+		fmt.Println(event.Asks[0].Price)
+		fmt.Println(event.Asks[0].Quantity)
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, _, err := futures.WsPartialDepthServe("BTCUSDT", "5", wsDepthHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// remove this if you do not want to be blocked here
+	<-doneC
+}
+
+func Test_CombinedPartial(t *testing.T) {
+	wsDepthHandler := func(event *futures.WsDepthEvent) {
+		fmt.Println(event.Asks[0].Price)
+		fmt.Println(event.Asks[0].Quantity)
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+
+	symbolLevels := map[string]string{
+		"BTCUSDT": "5",
+		"ETHUSDT": "5",
+	}
+	doneC, _, err := futures.WsCombinedPartialDepthServe(symbolLevels, wsDepthHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// remove this if you do not want to be blocked here
+	<-doneC
+}
+
+func Test_Kline(t *testing.T) {
+	wsKlineHandler := func(event *futures.WsKlineEvent) {
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, _, err := futures.WsKlineServe("BTCUSDT", "1m", wsKlineHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-doneC
+}
+
+func Test_AggTrade(t *testing.T) {
+	wsAggTradeHandler := func(event *futures.WsAggTradeEvent) {
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, _, err := futures.WsAggTradeServe("BTCUSDT", wsAggTradeHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-doneC
+}
+
+func Test_MarkPrice(t *testing.T) {
+	wsMarkPriceHandler := func(event *futures.WsMarkPriceEvent) {
+		fmt.Println(event)
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, _, err := futures.WsMarkPriceServe("BTCUSDT", wsMarkPriceHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-doneC
+}
+
+func Test_UserData(t *testing.T) {
+	listenKey := "pqia91ma19a5s61cv6a81va65sdf19v8a65a1a5s61cv6a81va65sdf19v8a65a1"
+	wsHandler := func(message []byte) {
+		fmt.Println(string(message))
+	}
+	errHandler := func(err error) {
+		fmt.Println(err)
+	}
+	doneC, _, err := futures.WsUserDataServe(listenKey, wsHandler, errHandler)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	<-doneC
+}
---
+++ trade/futures/websocket_service.go
@@ -0,0 +1,346 @@
+package futures
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+)
+
+var (
+	baseURL         = "wss://fstream.binancezh.com/ws"
+	combinedBaseURL = "wss://fstream.binancezh.com/stream?streams="
+	// WebsocketTimeout is an interval for sending ping/pong messages if WebsocketKeepalive is enabled
+	WebsocketTimeout = time.Second * 60
+	// WebsocketKeepalive enables sending ping/pong messages to check the connection stability
+	WebsocketKeepalive = true
+)
+
+// WsDepthHandler handle websocket depth event
+type WsDepthHandler func(event *WsDepthEvent)
+
+// WsDepthServe serve websocket depth handler with a symbol
+func WsDepthServe(symbol string, handler WsDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@depth", baseURL, strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		j, err := newJSON(message)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		event := new(WsDepthEvent)
+		event.Event = j.Get("e").MustString()
+		event.Time = j.Get("E").MustInt64()
+		event.TradeTime = j.Get("T").MustInt64()
+		event.Symbol = j.Get("s").MustString()
+		event.FirstUpdateID = j.Get("U").MustInt64()
+		event.LastUpdateID = j.Get("u").MustInt64()
+		event.PreviousID = j.Get("pu").MustInt64()
+		bidsLen := len(j.Get("b").MustArray())
+		event.Bids = make([]Bid, bidsLen)
+		for i := 0; i < bidsLen; i++ {
+			item := j.Get("b").GetIndex(i)
+			event.Bids[i] = Bid{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		asksLen := len(j.Get("a").MustArray())
+		event.Asks = make([]Ask, asksLen)
+		for i := 0; i < asksLen; i++ {
+			item := j.Get("a").GetIndex(i)
+			event.Asks[i] = Ask{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsPartialDepthServe serve websocket partial depth handler with a symbol
+func WsPartialDepthServe(symbol string, levels string, handler WsDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@depth%s", baseURL, strings.ToLower(symbol), levels)
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		j, err := newJSON(message)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		event := new(WsDepthEvent)
+		event.Event = j.Get("e").MustString()
+		event.Time = j.Get("E").MustInt64()
+		event.TradeTime = j.Get("T").MustInt64()
+		event.Symbol = j.Get("s").MustString()
+		event.FirstUpdateID = j.Get("U").MustInt64()
+		event.LastUpdateID = j.Get("u").MustInt64()
+		event.PreviousID = j.Get("pu").MustInt64()
+		bidsLen := len(j.Get("b").MustArray())
+		event.Bids = make([]Bid, bidsLen)
+		for i := 0; i < bidsLen; i++ {
+			item := j.Get("b").GetIndex(i)
+			event.Bids[i] = Bid{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		asksLen := len(j.Get("a").MustArray())
+		event.Asks = make([]Ask, asksLen)
+		for i := 0; i < asksLen; i++ {
+			item := j.Get("a").GetIndex(i)
+			event.Asks[i] = Ask{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsCombinedPartialDepthServe is similar to WsPartialDepthServe, but it for multiple symbols
+func WsCombinedPartialDepthServe(symbolLevels map[string]string, handler WsDepthHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := combinedBaseURL
+	for s, l := range symbolLevels {
+		endpoint += fmt.Sprintf("%s@depth%s", strings.ToLower(s), l) + "/"
+	}
+	endpoint = endpoint[:len(endpoint)-1]
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		j, err := newJSON(message)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		event := new(WsDepthEvent)
+		event.Event = j.Get("e").MustString()
+		event.Time = j.Get("E").MustInt64()
+		event.TradeTime = j.Get("T").MustInt64()
+		event.Symbol = j.Get("s").MustString()
+		event.FirstUpdateID = j.Get("U").MustInt64()
+		event.LastUpdateID = j.Get("u").MustInt64()
+		event.PreviousID = j.Get("pu").MustInt64()
+		bidsLen := len(j.Get("b").MustArray())
+		event.Bids = make([]Bid, bidsLen)
+		for i := 0; i < bidsLen; i++ {
+			item := j.Get("b").GetIndex(i)
+			event.Bids[i] = Bid{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		asksLen := len(j.Get("a").MustArray())
+		event.Asks = make([]Ask, asksLen)
+		for i := 0; i < asksLen; i++ {
+			item := j.Get("a").GetIndex(i)
+			event.Asks[i] = Ask{
+				Price:    item.GetIndex(0).MustString(),
+				Quantity: item.GetIndex(1).MustString(),
+			}
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsDepthEvent define websocket depth event
+type WsDepthEvent struct {
+	Event         string `json:"e"`
+	Time          int64  `json:"E"`
+	TradeTime     int64  `json:"T"`
+	Symbol        string `json:"s"`
+	FirstUpdateID int64  `json:"U"`
+	LastUpdateID  int64  `json:"u"`
+	PreviousID    int64  `json:"pu"`
+	Bids          []Bid  `json:"b"`
+	Asks          []Ask  `json:"a"`
+}
+
+// WsKlineHandler handle websocket kline event
+type WsKlineHandler func(event *WsKlineEvent)
+
+// WsKlineServe serve websocket kline handler with a symbol and interval like 15m, 30s
+func WsKlineServe(symbol string, interval string, handler WsKlineHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@kline_%s", baseURL, strings.ToLower(symbol), interval)
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		event := new(WsKlineEvent)
+		err := json.Unmarshal(message, event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsKlineEvent define websocket kline event
+type WsKlineEvent struct {
+	Event  string  `json:"e"`
+	Time   int64   `json:"E"`
+	Symbol string  `json:"s"`
+	Kline  WsKline `json:"k"`
+}
+
+// WsKline define websocket kline
+type WsKline struct {
+	StartTime            int64  `json:"t"`
+	EndTime              int64  `json:"T"`
+	Symbol               string `json:"s"`
+	Interval             string `json:"i"`
+	FirstTradeID         int64  `json:"f"`
+	LastTradeID          int64  `json:"L"`
+	Open                 string `json:"o"`
+	Close                string `json:"c"`
+	High                 string `json:"h"`
+	Low                  string `json:"l"`
+	Volume               string `json:"v"`
+	TradeNum             int64  `json:"n"`
+	IsFinal              bool   `json:"x"`
+	QuoteVolume          string `json:"q"`
+	ActiveBuyVolume      string `json:"V"`
+	ActiveBuyQuoteVolume string `json:"Q"`
+}
+
+// WsAggTradeHandler handle websocket aggregate binance event
+type WsAggTradeHandler func(event *WsAggTradeEvent)
+
+// WsAggTradeServe serve websocket aggregate handler with a symbol
+func WsAggTradeServe(symbol string, handler WsAggTradeHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@aggTrade", baseURL, strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		event := new(WsAggTradeEvent)
+		err := json.Unmarshal(message, event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsAggTradeEvent define websocket aggregate binance event
+type WsAggTradeEvent struct {
+	Event                 string `json:"e"`
+	Time                  int64  `json:"E"`
+	Symbol                string `json:"s"`
+	AggTradeID            int64  `json:"a"`
+	Price                 string `json:"p"`
+	Quantity              string `json:"q"`
+	FirstBreakdownTradeID int64  `json:"f"`
+	LastBreakdownTradeID  int64  `json:"l"`
+	TradeTime             int64  `json:"T"`
+	IsBuyerMaker          bool   `json:"m"`
+	Placeholder           bool   `json:"M"` // add this field to avoid case insensitive unmarshaling
+}
+
+// WsMarkPriceHandler handle websocket binance event
+type WsMarkPriceHandler func(event *WsMarkPriceEvent)
+
+// WsMarkPriceServe serve websocket handler with a symbol
+func WsMarkPriceServe(symbol string, handler WsMarkPriceHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@markPrice", baseURL, strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		event := new(WsMarkPriceEvent)
+		err := json.Unmarshal(message, event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsMarkPriceEvent define websocket binance event
+type WsMarkPriceEvent struct {
+	Event       string `json:"e"`
+	Time        int64  `json:"E"`
+	Symbol      string `json:"s"`
+	Price       string `json:"p"`
+	TradeTime   int64  `json:"T"`
+	FundingRate string `json:"r"`
+	Placeholder bool   `json:"M"` // add this field to avoid case insensitive unmarshaling
+}
+
+// WsUserDataServe serve user data handler with listen key
+func WsUserDataServe(listenKey string, handler WsHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s", baseURL, listenKey)
+	cfg := newWsConfig(endpoint)
+	return wsServe(cfg, handler, errHandler)
+}
+
+// WsMarketStatHandler handle websocket that push single market statistics for 24hr
+type WsMarketStatHandler func(event *WsMarketStatEvent)
+
+// WsMarketStatServe serve websocket that push 24hr statistics for single market every second
+func WsMarketStatServe(symbol string, handler WsMarketStatHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/%s@ticker", baseURL, strings.ToLower(symbol))
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		var event WsMarketStatEvent
+		err := json.Unmarshal(message, &event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(&event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsAllMarketsStatHandler handle websocket that push all markets statistics for 24hr
+type WsAllMarketsStatHandler func(event WsAllMarketsStatEvent)
+
+// WsAllMarketsStatServe serve websocket that push 24hr statistics for all market every second
+func WsAllMarketsStatServe(handler WsAllMarketsStatHandler, errHandler ErrHandler) (doneC, stopC chan struct{}, err error) {
+	endpoint := fmt.Sprintf("%s/!ticker@arr", baseURL)
+	cfg := newWsConfig(endpoint)
+	wsHandler := func(message []byte) {
+		var event WsAllMarketsStatEvent
+		err := json.Unmarshal(message, &event)
+		if err != nil {
+			errHandler(err)
+			return
+		}
+		handler(event)
+	}
+	return wsServe(cfg, wsHandler, errHandler)
+}
+
+// WsAllMarketsStatEvent define array of websocket market statistics events
+type WsAllMarketsStatEvent []*WsMarketStatEvent
+
+// WsMarketStatEvent define websocket market statistics event
+type WsMarketStatEvent struct {
+	Event              string `json:"e"`
+	Time               int64  `json:"E"`
+	Symbol             string `json:"s"`
+	PriceChange        string `json:"p"`
+	PriceChangePercent string `json:"P"`
+	WeightedAvgPrice   string `json:"w"`
+	PrevClosePrice     string `json:"x"`
+	LastPrice          string `json:"c"`
+	CloseQty           string `json:"Q"`
+	BidPrice           string `json:"b"`
+	BidQty             string `json:"B"`
+	AskPrice           string `json:"a"`
+	AskQty             string `json:"A"`
+	OpenPrice          string `json:"o"`
+	HighPrice          string `json:"h"`
+	LowPrice           string `json:"l"`
+	BaseVolume         string `json:"v"`
+	QuoteVolume        string `json:"q"`
+	OpenTime           int64  `json:"O"`
+	CloseTime          int64  `json:"C"`
+	FirstID            int64  `json:"F"`
+	LastID             int64  `json:"L"`
+	Count              int64  `json:"n"`
+}
